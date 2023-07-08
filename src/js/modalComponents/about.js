64f3ad4469be43b56510bbae16d2a0093d586378
Suphakit P <me@suphakit.net>
2023-07-08 12:50:58 +0700

    feat<modal>: about data

--- src/js/modalComponents/about.js
+++ src/js/modalComponents/about.js
@@ -2,6 +2,52 @@ const modal = document.querySelector('#AboutModal')
 
 const age = new Date().getFullYear() - new Date('June 15, 2002').getFullYear()
 
+const eduData = [
+  {
+    grade: "Kindergarten - Grade 6",
+    class: "",
+    school: "Darasamutr Sriracha School",
+    year: "2005 - 2014"
+  },
+  {
+    grade: "Grade 7-9",
+    class: "MEP",
+    school: "Sriracha School",
+    year: "2014 - 2018"
+  },
+  {
+    grade: "Grade 10",
+    class: "Sci-math",
+    school: "Piboonbumpen Demonstration School",
+    year: "2018 - 2019"
+  },
+  {
+    grade: "Grade 11-12",
+    class: "Sci-Math",
+    school: "Darasamutr Sriracha School",
+    year: "2019 - April 2021"
+  },
+  {
+    grade: "Bachelor's degree",
+    class: "ICT - Computer Engineering",
+    school: "University of Phayao",
+    year: "May 2021 - Present"
+  }
+]
+
+let eduBox = ""
+eduData.forEach((data) => {
+  const Box = `<li>
+  [${data.grade}] - ${(data.class != "") ? `${data.class} @` : ""} ${data.school} [${data.year}]
+</li>`
+
+  if (eduBox != "") {
+    eduBox = eduBox + Box
+  } else {
+    eduBox = Box
+  }
+})
+
 modal.innerHTML = `<div class="contentContainer">
   <h1 style="text-decoration: underline;">
     About
@@ -17,6 +63,13 @@ modal.innerHTML = `<div class="contentContainer">
       <li>
         <strong>Age: </strong> ${age}
       </li>
+    </ul>
+    <br> 
+    <h3 style="text-decoration: underline;">
+      Educational profile
+    </h3>
+    <ul>
+      ${eduBox}
     </ul>
   </div>
 </div>`
