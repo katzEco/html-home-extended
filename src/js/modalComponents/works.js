5d829676dcb6d46603ab5ac3cfb33311ee6bf68f
Suphakit P <me@suphakit.net>
2023-07-09 20:35:12 +0700

    feat<work_modal>: setting modal frame

---
+++ src/js/modalComponents/works.js
@@ -0,0 +1,15 @@
+const worksData = document.querySelector('#WorksModal')
+
+worksData.innerHTML = `<div class="contentContainer">
+  <h1 style="text-decoration: underline;">
+    Works
+  </h1>
+
+  <h1 style="text-decoration: underline;">
+    Joined Events
+  </h1>
+
+  <h1 style="text-decoration: underline;">
+    Certificates
+  </h1>
+</div>`
