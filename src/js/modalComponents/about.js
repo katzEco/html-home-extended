e7549a965b435d73eaf63a24d0d873509ccc1196
Suphakit P <me@suphakit.net>
2023-07-11 12:53:17 +0700

    feat<modal>: moved skilling from works -> about

--- src/js/modalComponents/about.js
+++ src/js/modalComponents/about.js
@@ -54,6 +54,60 @@ eduData.forEach((data) => {
   }
 })
 
+// Skill data
+const skilling = {
+  programming: [
+    'HTML5',
+    'CSS3',
+    'Bootstrap',
+    'Tailwind.css',
+    'PHP',
+    'Javascript',
+    'Typescript',
+    'React.js',
+    'Vue.js',
+    'Nuxt.js',
+    'Express.js',
+    'Hapi.js',
+    'Hexo'
+  ],
+  musicComposing: [
+    'Producing',
+    'Vocaling',
+    'Mastering',
+    'Composing',
+    'Arranging'
+  ]
+}
+
+// Programming Skill foreaching
+let programBox = ''
+skilling.programming.forEach((programming) => {
+  const based = `<p>
+  ${programming}
+</p>`
+
+  if (programBox == '') {
+    programBox = based
+  } else {
+    programBox = programBox + based
+  }
+})
+
+// Music composing Skill foreaching
+let musicBox = ''
+skilling.musicComposing.forEach((music) => {
+  const based = `<p>
+  ${music}
+</p>`
+
+  if (musicBox == '') {
+    musicBox = based
+  } else {
+    musicBox = musicBox + based
+  }
+})
+
 aboutData.innerHTML = `<div class="contentContainer">
   <h1 style="text-decoration: underline;">
     About
@@ -78,4 +132,25 @@ aboutData.innerHTML = `<div class="contentContainer">
       ${eduBox}
     </ul>
   </div>
+
+  <h1 style="text-decoration: underline;">
+    Skills
+  </h1>
+  <div class="detailed">
+    <h3 style="text-decoration: underline;">
+      Programming
+    </h3>
+
+    <div class="buttonBox">
+      ${programBox}
+    </div>
+
+    <h3 style="text-decoration: underline;">
+      Music Composing
+    </h3>
+
+    <div class="buttonBox">
+      ${musicBox}
+    </div>
+  </div> 
 </div>`
--- src/js/modalComponents/works.js
+++ src/js/modalComponents/works.js
@@ -1,80 +1,9 @@
 const worksData = document.querySelector('#WorksModal')
 
-// Skill data
-const skilling = {
-  programming: [
-    'HTML5',
-    'CSS3',
-    'Bootstrap',
-    'Tailwind.css',
-    'PHP',
-    'Javascript',
-    'Typescript',
-    'React.js',
-    'Vue.js',
-    'Nuxt.js',
-    'Express.js',
-    'Hapi.js',
-    'Hexo'
-  ],
-  musicComposing: [
-    'Producing',
-    'Vocaling',
-    'Mastering',
-    'Composing',
-    'Arranging'
-  ]
-}
-
-// Programming Skill foreaching
-let programBox = ''
-skilling.programming.forEach((programming) => {
-  const based = `<p>
-  ${programming}
-</p>`
-
-  if (programBox == '') {
-    programBox = based
-  } else {
-    programBox = programBox + based
-  }
-})
-
-// Music composing Skill foreaching
-let musicBox = ''
-skilling.musicComposing.forEach((music) => {
-  const based = `<p>
-  ${music}
-</p>`
-
-  if (musicBox == '') {
-    musicBox = based
-  } else {
-    musicBox = musicBox + based
-  }
-})
-
 worksData.innerHTML = `<div class="contentContainer">
   <h1 style="text-decoration: underline;">
     Works
   </h1>
-  <div class="detailed">
-    <h3 style="text-decoration: underline;">
-      Programming
-    </h3>
-
-    <div class="buttonBox">
-      ${programBox}
-    </div>
-
-    <h3 style="text-decoration: underline;">
-      Music Composing
-    </h3>
-
-    <div class="buttonBox">
-      ${musicBox}
-    </div>
-  </div> 
 
   <h1 style="text-decoration: underline;">
     Joined Events
