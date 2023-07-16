00bce2e9546332312a82b2d7735a37b88b761dca
Suphakit P <me@suphakit.net>
2023-07-16 20:30:47 +0700

    feat<various>: status modal changes

--- src/js/modalComponents/status.js
+++ src/js/modalComponents/status.js
@@ -33,6 +33,64 @@ async function lanyardPull() {
 
 async function mainLanyard() {
   const modal = document.querySelector('#StatusModal')
+  const API = await lanyardPull()
+
+  let status
+  switch (API.status) {
+    case 'online':
+      status = "Online"
+      break;
+    
+    case 'idle':
+      status = "Idle"
+      break;
+
+    case 'dnd':
+      status = "Do not disturb"
+      break;
+  
+    default:
+      status = "Offline"
+      break;
+  }
+
+  let spotifyElement
+  if (API.listen == true) {
+    spotifyElement = `<a class="card" href="${API.spotify.trackLink}">
+  <div class="cardBody">
+    <h3>
+      ${API.spotify.song}
+    </h3>
+    <h4>
+      ${API.spotify.artist}
+    </h4>
+    <p>
+      ${API.spotify.album}
+    </p>
+  </div>
+  <div class="img" style="background: url(${API.spotify.albumArt});"></div>
+</a>`
+  } else {
+    spotifyElement = ""
+  }
+
+  modal.innerHTML = `<div class="contentContainer">
+  <div class="cardContainer">
+    <div class="card">
+      <div class="img" style="background: url(${API.avatarImage});"></div>
+      <div class="cardBody">
+        <h3>
+          ${API.name} － ${status}
+        </h3>
+        <p>
+          ${(API.customStatus) != null ? API.customStatus : "No status now.."}
+        </p>
+      </div>
+    </div>
+
+    ${spotifyElement}
+  </div> 
+</div>`
 
   console.log(await lanyardPull());
 }
